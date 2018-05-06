2620f91cf60cb8907b34f89dbb817ad679eb7249
Manish Shambu <manishshambu@gmail.com>
2018-05-06 16:46:48 -0600

    Update sms_subsciber.py

--- sms_subsciber.py
+++ sms_subsciber.py
@@ -13,13 +13,13 @@ def message_handler(client, userdata, message):
 		send_sms()
 
 def send_sms():
-	account_sid = "AC5b9818960edf481a6bbfcc2416ccb324"
-	auth_token = "4d1627c8154a9c14c43c38f52575861a"
+	account_sid = ""
+	auth_token = ""
 	
 	client = Client(account_sid, auth_token)
 
 	message = client.messages.create(
-		to = "+17204924454",
+		to = "+",
 		from_ = "+19389999211",
 		body = "Alert - Intrusion detected at house!")
 	
