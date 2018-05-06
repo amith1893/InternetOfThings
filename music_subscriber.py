42f11177b4340ee7cd20471c14bef136dd066eff
amith <amith1893@gmail.com>
2018-05-06 10:21:50 -0600

    Updated the subscriber with asynchronous music playing

--- music_subscriber.py
+++ music_subscriber.py
@@ -2,6 +2,10 @@ import paho.mqtt.client as mqtt
 #import broker
 import time
 import pygame
+import os
+import psutil
+
+play_f = 0
 
 class Sound:
     def __init__(self):
@@ -18,25 +22,42 @@ class Sound:
         pygame.mixer.music.stop()
 
 
+def play():
+    sound = Sound()
+    sound.playSound()
+
 def message_handler(client, userdata, message):
     print("Message received ", str(message.payload.decode("utf-8")))
     print("Message topic ", message.topic)
     print("Message QOS ", message.qos)
     print("Message retain flag ", message.retain)
-    sound = Sound()
     print(message)
     command = str(message.payload.decode("utf-8"))
+    
+    global playf
     if command == "Play":
-        print("---------------------------")
-        sound.playSound()
+        print("---------------------------------------")
+        if playf == 0:
+            pid = os.fork()
+            playf = 1
+            if pid == 0:
+                sound.playSound()
+        else:
+            print("Already playing")
     elif message == "Stop":
-        sound.stopSound()
+        if playf == 1:
+            print("Need to Stop")
+            print("Stop the existing child who is playing music")
+            playf = 0
+            curr_id = psutil.Process(os.getpid())
+            for child in curr_id.children(recursive=True):
+                child.kill()
+        else:
+            print("Not running any file. No need to stop")
        
-
 
 subs = mqtt.Client("subscriber-3")
 print("Connecting to broker")
-#subs.connect("127.0.0.1:1883")
 subs.connect("127.0.0.1")
 print("Subscribing to music topic")
 subs.on_message = message_handler
